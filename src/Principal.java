b4281a590345f14b30d227a537f5a3594757b446
Terezinha Freire C Sousa <terezafcsousa@gmail.com>
2025-04-30 18:18:23 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+        System.out.println("Digite um número de CEP para consulta:");
+        var cep = leitura.nextLine();
+        try{
+            Endereco novoEndeco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndeco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndeco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+    }
 }
